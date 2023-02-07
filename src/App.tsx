358e6dc8c69117ea18fae6a9cc3e2afabdc15490
TinsFox <menhuluy@gmail.com>
2023-02-07 20:41:24 +0800

    feat: 修正公式,ui优化

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,25 @@ import {
   Button,
   Container,
   VStack,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
   Input,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React, { useState, useMemo } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
@@ -53,6 +71,7 @@ type Inputs = {
   endStation: string;
 };
 export default function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [businessObject, setBusinessObject] = useState<BusinessObject[]>([]);
   const initData = async () => {
     const res = await fetch(
@@ -75,7 +94,11 @@ export default function App() {
     trigger,
     formState: { errors },
   } = useForm<Inputs>();
+  const [timeCoast, setTimeCoast] = useState(0);
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    if (data.numberOfDays <= 0) {
+      alert("不要捣乱，居家办公或者去找工作去");
+    }
     console.log(data);
     const res = await fetch(
       `https://apis.gzmtr.com/app-map/metroweb/route/${data.startStation}/${data.endStation}`,
@@ -84,19 +107,31 @@ export default function App() {
       }
     );
     const data1 = await res.json();
-    console.log("data1", data1.businessObject.price);
+    console.log("时间", data1.businessObject.routes[0].metro[0].spend_time);
+
+    console.log(
+      "data1",
+      data1.businessObject.routes[0].metro[0].spend_time / 60
+    );
+    setTimeCoast(data1.businessObject.routes[0].metro[0].spend_time / 60);
     setPrice(data1.businessObject.price);
+    onOpen();
   };
   const watchFields = watch([
-    "start",
-    "end",
-    "numberOfDays",
-    "startStation",
-    "endStation",
+    "start", //0
+    "end", //1
+    "numberOfDays", //2
+    "startStation", //3
+    "endStation", //4
   ]);
-
+  // 单价*15 + （天数 * 2 - 15）* 单价 * 0.6
   const totalCoast = useMemo(() => {
-    return price * 16 + price * (watchFields[2] * 2 - 16) * 0.6;
+    const total = watchFields[2] * 2;
+    if (total > 15) {
+      return price * 15 + price * (total - 15) * 0.6;
+    } else {
+      return price * total;
+    }
   }, [price, watchFields]);
 
   React.useEffect(() => {
@@ -106,55 +141,79 @@ export default function App() {
     return () => subscription.unsubscribe();
   }, [watch]);
   return (
-    <VStack>
-      <Container maxW="550px">
-        <Card>
+    <>
+      <VStack width="100vw" mt={10}>
+        <Card minW="330">
           <CardHeader>
-            <Heading size="md">漂流小屋</Heading>
+            <Heading size="md">打工账本</Heading>
           </CardHeader>
           <CardBody>
-            <Stack divider={<StackDivider />} spacing="4">
-              <Box>
-                <Heading size="xs" textTransform="uppercase">
-                  通勤费用计算
-                </Heading>
-                <Text pt="2" fontSize="sm">
-                  根据起始地铁站结合上班天数，计算通勤花费
-                </Text>
-              </Box>
-              <Box>
-                <Text pt="2" fontSize="sm" as={"div"}>
-                  <div>
-                    {watchFields[3]}
-                    {"=>"} {watchFields[4]} 总花费{totalCoast}
-                  </div>
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <FormControl>
-                      <FormLabel>上班天数</FormLabel>
-                      <Input
-                        type="number"
-                        {...register("numberOfDays", { required: true })}
-                      />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel>选择出发地铁站</FormLabel>
-                      <Select
-                        placeholder="选择出发地铁站"
-                        {...register("start", { required: true })}
-                      >
-                        {businessObject?.map((item) => (
-                          <option key={item.lineId} value={item.lineName}>
-                            {item.lineName}
-                          </option>
-                        ))}
-                      </Select>
-                    </FormControl>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                通勤费用计算
+              </Heading>
+              <Accordion allowMultiple>
+                <AccordionItem>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      使用方法
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                  <AccordionPanel pb={4}>
+                    <OrderedList>
+                      <ListItem>输入上班天数</ListItem>
+                      <ListItem>选择地铁起始站点</ListItem>
+                      <ListItem>开始计算</ListItem>
+                    </OrderedList>
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      计算规则
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                  <AccordionPanel pb={4}>
+                    <OrderedList>
+                      <ListItem>通勤次数=上班天数*2</ListItem>
+                      <ListItem>16次原价计算</ListItem>
+                      <ListItem>16次后打六折</ListItem>
+                    </OrderedList>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </Box>
+            <Box>
+              <Text pt="2" fontSize="sm" as={"div"}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <FormControl>
+                    <FormLabel>上班天数</FormLabel>
+                    <Input
+                      type="number"
+                      {...register("numberOfDays", { required: true })}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>选择出发地铁站：{watchFields[3]}</FormLabel>
+                    <Select
+                      placeholder="选择出发地铁站"
+                      {...register("start", { required: true })}
+                    >
+                      {businessObject?.map((item) => (
+                        <option key={item.lineId} value={item.lineName}>
+                          {item.lineName}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  {watchFields[0] && (
                     <Box
                       display={"flex"}
-                      w={450}
-                      h={100}
                       overflow={"auto"}
                       flexWrap={"wrap"}
+                      h={100}
                     >
                       {businessObject
                         ?.find((item) => item.lineName === watchFields[0])
@@ -171,21 +230,24 @@ export default function App() {
                           </Button>
                         ))}
                     </Box>
-                    <FormControl>
-                      <FormLabel>选择目标地铁站</FormLabel>
-                      <Select
-                        placeholder="选择目标地铁站"
-                        {...register("end", { required: true })}
-                      >
-                        {businessObject?.map((item) => (
-                          <option key={item.lineId} value={item.lineName}>
-                            {item.lineName}
-                          </option>
-                        ))}
-                      </Select>
+                  )}
+
+                  <FormControl>
+                    <FormLabel>选择目标地铁站：{watchFields[4]}</FormLabel>
+                    <Select
+                      placeholder="选择目标地铁站"
+                      {...register("end", { required: true })}
+                    >
+                      {businessObject?.map((item) => (
+                        <option key={item.lineId} value={item.lineName}>
+                          {item.lineName}
+                        </option>
+                      ))}
+                    </Select>
+                    {watchFields[1] && (
                       <Box
                         display={"flex"}
-                        w={450}
+                        maxW="100vw"
                         h={100}
                         overflow={"auto"}
                         flexWrap={"wrap"}
@@ -205,16 +267,62 @@ export default function App() {
                             </Button>
                           ))}
                       </Box>
-                    </FormControl>
-
-                    <Button type="submit">计算通勤花费</Button>
-                  </form>
-                </Text>
-              </Box>
-            </Stack>
+                    )}
+                  </FormControl>
+                  <Button type="submit" m={"0 auto"} display="block" mt="15">
+                    计算通勤花费
+                  </Button>
+                </form>
+              </Text>
+            </Box>
           </CardBody>
         </Card>
-      </Container>
-    </VStack>
+      </VStack>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>通勤花费</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box display={"flex"} justifyContent="center" mb="10">
+              <Box>
+                <Text mr="5">出发站：{watchFields[3]}</Text>
+                <Text>目的地：{watchFields[4]}</Text>
+              </Box>
+              <Box>
+                <Text mr="5">单程花费：{price}元</Text>
+                <Text>总花费：{totalCoast}元</Text>
+              </Box>
+            </Box>
+            <Box>
+              <Text mr="5">单程耗时：{timeCoast}分钟</Text>
+              <Text>每天耗时：{timeCoast * 2}分钟</Text>
+              <Text>
+                一个月你要在地铁上：{timeCoast * watchFields[2] * 2}分钟
+              </Text>
+            </Box>
+            <Box textAlign={"center"}>
+              计算公式：
+              <span>
+                {watchFields[2] * 2 > 15 ? (
+                  <span>
+                    {price} * 15 + {price} * ({watchFields[2]} * 2 - 15) * 0.6
+                  </span>
+                ) : (
+                  <span>
+                    {price} * ({watchFields[2]} * 2 )
+                  </span>
+                )}
+              </span>
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              关闭
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
